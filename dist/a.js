e17d65d29d037cd71d6512efe916cae081fb0e46
Vratesh <ghadgevratesh@gmail.com>
2026-02-21 16:50:41 +0530

    TS-Basics

--- dist/a.js
+++ dist/a.js
@@ -6,9 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //     console.log("Hello" + firstName)
 // }
 // greet("AJ");
-function sum(a, b) {
-    return a + b;
+//type infrernce
+// function sum(a: number, b: number){
+//     return a + b;
+// }
+// const value = sum(2,5);
+// console.log(value)
+function isLegal(age) {
+    if (age > 18) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-const value = sum(2, 5);
-console.log(value);
+let x = isLegal(18);
+console.log(x);
 //# sourceMappingURL=a.js.map
